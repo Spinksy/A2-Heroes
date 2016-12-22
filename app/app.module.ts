bf4828b75dbdec18cc2ea894a5d3d84727753626
Glenn Spinks <glennspinks@gmail..com>
2016-12-22 09:53:21 +0800

    Add in memory data service and http module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
 import { DashboardComponent, HeroComponent, HeroDetailComponent, HeroListComponent } from './components/component.index';
-import { HeroService } from './shared/services/hero.service';
+import { HeroService }   from './shared/services/hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [
     AppComponent,
@@ -22,12 +29,8 @@ import { AppRoutingModule } from './app-routing.module';
     HeroDetailComponent,
     HeroListComponent
     ],
-    providers: [
-      HeroService
-    ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
 
